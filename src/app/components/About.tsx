22d37feb66c0fce3b3d3bcaf508683c7bbc92f22
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-05-29 21:40:23 -0300

    actualizacion

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -32,7 +32,7 @@ const About = () => {
            ¿Quien es Fernando Rampazzo?
           </h1>
           <p className="md:pb-[4.44vh] pb-7">
-          Fernando Rampazzo es abogado especialista en <span className="md:text-[1.66vh] text-base leading-[22px] font-bold">Derecho Provisional y laboral</span>. Asesora a adultos mayores y trabajadores, 
+          Fernando Rampazzo es abogado especialista en <span className="md:text-[1.66vh] text-base leading-[22px] font-bold">derecho previsional y laboral</span>. Asesora a adultos mayores y trabajadores, 
           con mas de 15 años de trayectoria.
 
           Difunde los derechos de los jubilados y pensionados en los medios de comunicacion Nacionales.
